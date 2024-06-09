cc05d13a06813af441bde0cb837dc5b533c59eb7
Oliver Loftus <o@oloft.us>
2024-06-09 17:37:09 +0000

    Add extra OK case

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { createSelector } from "reselect";
 
 type A = { a: string };
 type B = { a: string; b: string };
+type C = { c: string };
 
 // const selectorOK: () => ((state: {
 //   it: Record<string, A | B>;
@@ -17,10 +18,29 @@ type B = { a: string; b: string };
 //   ...;
 // }
 
-const selectorOK = () =>
+const selectorOK1 = () =>
   createSelector(
     [(state: { it: Record<string, A | B> }) => state.it],
     (it) => it["y"]
+  );
+
+// const selectorOK2: (y: string | undefined) => ((state: {
+//     it: Record<string, A | C>;
+// }) => A | C | undefined) & {
+//     clearCache: () => void;
+//     resultsCount: () => number;
+//     resetResultsCount: () => void;
+// } & {
+//     resultFunc: (resultFuncArgs_0: Record<string, A | C>) => A | ... 1 more ... | undefined;
+//     ... 6 more ...;
+//     resetDependencyRecomputations: () => void;
+// } & {
+//     ...;
+// }
+
+const selectorOK2 = (y: string | undefined) =>
+  createSelector([(state: { it: Record<string, A | C> }) => state.it], (it) =>
+    y !== undefined ? it[y] : undefined
   );
 
 // const selectorNOK: (y: string | undefined) => ((state: {
