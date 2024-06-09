c1ef195bbab4d48926598c380fa7a2232a1aadc6
Oliver Loftus <o@oloft.us>
2024-06-09 17:37:09 +0000

    Update calculated type comments

--- src/index.tsx
+++ src/index.tsx
@@ -2,35 +2,55 @@ import { createSelector } from "reselect";
 
 type A = { a: string };
 type B = { a: string; b: string };
+type C = { c: string };
 
-// const selectorOK: () => ((state: {
+// const selectorOK1: () => ((state: {
 //   it: Record<string, A | B>;
-// }) => A | B) & {
+// }) => A | B | undefined) & {
 //   clearCache: () => void;
 //   resultsCount: () => number;
 //   resetResultsCount: () => void;
 // } & {
-//   resultFunc: (resultFuncArgs_0: Record<string, A | B>) => A | B;
+//   resultFunc: (resultFuncArgs_0: Record<string, A | B>) => A | ... 1 more ... | undefined;
 //   ... 6 more ...;
 //   resetDependencyRecomputations: () => void;
 // } & {
 //   ...;
 // }
 
-const selectorOK = () =>
+const selectorOK1 = () =>
   createSelector(
     [(state: { it: Record<string, A | B> }) => state.it],
     (it) => it["y"]
   );
 
-// const selectorNOK: (y: string | undefined) => ((state: {
-//     it: Record<string, A | B>;
-// }) => A) & {
+// const selectorOK2: (y: string | undefined) => ((state: {
+//     it: Record<string, A | C>;
+// }) => A | C | undefined) & {
 //     clearCache: () => void;
 //     resultsCount: () => number;
 //     resetResultsCount: () => void;
 // } & {
-//     resultFunc: (resultFuncArgs_0: Record<string, A | B>) => A;
+//     resultFunc: (resultFuncArgs_0: Record<string, A | C>) => A | ... 1 more ... | undefined;
+//     ... 6 more ...;
+//     resetDependencyRecomputations: () => void;
+// } & {
+//     ...;
+// }
+
+const selectorOK2 = (y: string | undefined) =>
+  createSelector([(state: { it: Record<string, A | C> }) => state.it], (it) =>
+    y !== undefined ? it[y] : undefined
+  );
+
+// const selectorNOK: (y: string | undefined) => ((state: {
+//     it: Record<string, A | B>;
+// }) => A | undefined) & {
+//     clearCache: () => void;
+//     resultsCount: () => number;
+//     resetResultsCount: () => void;
+// } & {
+//     resultFunc: (resultFuncArgs_0: Record<string, A | B>) => A | undefined;
 //     ... 6 more ...;
 //     resetDependencyRecomputations: () => void;
 // } & {
